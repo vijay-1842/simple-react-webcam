a747cd3ea76e1a719f09476bdfbcfb449d6e1e4c
Vijay <mvijayapk@gmail.com>
2024-08-28 20:30:50 +0530

    feat: added image capture functionality

--- src/index.js
+++ src/index.js
@@ -1,44 +1,89 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Component } from "react";
 
-function SimpleReactWebcam(props) {
-    const {
-        requestDeclinedCallback = () => {
-            alert("Please Refresh the Page and Allow Camera Permission");
-        },
-        className = '',
-    } = props;
+class SimpleReactWebcam extends Component {
+  constructor(props) {
+    super(props);
 
-    const [isCameraShared, setCameraShared] = useState(false);
-    const camRef = useRef();
-
-    const requestCameraPermission = async () => {
-        const constraints = {
-            audio: false, video: { facingMode: 'user' },
-        };
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia(constraints);
-            camRef.current.srcObject = stream;
-            setCameraShared(true);
-        } catch (error) {
-            console.error(error);
-            requestDeclinedCallback(error);
-        }
+    this.state = {
+      isCameraShared: false,
+      videoDimensions: { width: 0, height: 0 },
     };
 
-    useEffect(() => {
-        requestCameraPermission();
-    }, [])
+    this.camRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.requestCameraPermission();
+  }
+
+  requestCameraPermission = async () => {
+    const {
+      requestDeclinedCallback = this.defaultDeclinedCallback,
+      includeAudio = false,
+    } = this.props;
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: includeAudio,
+        video: { facingMode: "user" },
+      });
+      this.camRef.current.srcObject = stream;
+
+      this.camRef.current.onloadedmetadata = this.handleMetadataLoad;
+    } catch (error) {
+      console.error(error);
+      requestDeclinedCallback(error);
+    }
+  };
+
+  handleMetadataLoad = () => {
+    const { videoWidth: width, videoHeight: height } = this.camRef.current;
+    this.setState({ isCameraShared: true, videoDimensions: { width, height } });
+  };
+
+  getCapturedImage = () => {
+    const { videoDimensions } = this.state;
+    const videoElem = this.camRef.current;
+
+    const canvas = document.createElement("canvas");
+    canvas.width = videoDimensions.width;
+    canvas.height = videoDimensions.height;
+
+    const context = canvas.getContext("2d");
+    context.drawImage(
+      videoElem,
+      0,
+      0,
+      videoDimensions.width,
+      videoDimensions.height
+    );
+    const image = canvas.toDataURL("image/png");
+
+    return image;
+  };
+
+  defaultDeclinedCallback = () => {
+    alert("Please Refresh the Page and Allow Camera Permission");
+  };
+
+  render() {
+    const { className = "", children } = this.props;
+    const { isCameraShared } = this.state;
 
     return (
+      <>
         <video
-            ref={camRef}
-            autoPlay
-            style={isCameraShared ? {} : { display: 'none' }}
-            className={className}
+          ref={this.camRef}
+          autoPlay
+          style={isCameraShared ? {} : { display: "none" }}
+          className={className}
         >
-            <track kind="captions" />
+          <track kind="captions" />
         </video>
+        {children && children({ getCapturedImage: this.getCapturedImage })}
+      </>
     );
+  }
 }
 
 export default SimpleReactWebcam;
